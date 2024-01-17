048eba04a63b7dc080442ecde6f17247d2b8fe39
Arvik1982 <phone.kulikovarseniy@gmail.com>
2024-01-17 14:38:58 +0300

    last changes main page

---
+++ src/pages/ChangePassword.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import styles from'./css/authorization.module.css';
+import logo from '../img/logo_auth.png'
+
+
+export default function ChangePasswordPage(){
+     const [changeElement, setChangeElement]=useState('login')
+
+    
+     return(
+    
+        changeElement==='login'?  
+        <div className={styles.authorization__page}>
+    
+        <div className={styles.authorization__page_logo}>
+        <img src={logo} alt="logo" />
+        </div>
+        
+        <div className={styles.authorization__page_inputs}>
+        <h4 className={styles.page__inputs_header}>Новый логин:</h4>
+            <input className={styles.page_input} type="text" placeholder='Логин' />
+        </div>
+        <div className={styles.authorization__page_buttons}>
+                <button onClick={changeElement==='login'?()=>setChangeElement('pass'):()=>setChangeElement('login')} 
+                type='button' className={styles.page__button_enter}>Сохранить</button>
+        </div>
+</div>:
+        <div className={styles.authorization__page}>
+    
+        <div className={styles.authorization__page_logo}>
+        <img src={logo} alt="logo" />
+        </div>
+
+        <div className={styles.authorization__page_inputs}>
+        <h4 className={styles.page__inputs_header}>Новый пароль:</h4>
+        <input className={styles.page_input} type="text" placeholder='Пароль' />
+        <input className={styles.page_input} type="text" placeholder='Повторите пароль' />
+        </div>
+        <div className={styles.authorization__page_buttons}>
+        <button onClick={changeElement==='login'?()=>setChangeElement('pass'):()=>setChangeElement('login')} 
+        type='button' className={styles.page__button_enter}>Сохранить</button>
+    </div>
+    
+</div>
+    
+    )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import MyProfilePage from "./pages/Myprofile";
 import ErrPage  from "./pages/Err";
 import AuthorizationPage from "./pages/Authorization";
 import DescriptionPage from "./pages/Ioga";
+import ChangePasswordPage from "./pages/ChangePassword";
 
 export default function AppRoutes () {
     return(
@@ -11,6 +12,7 @@ export default function AppRoutes () {
             <Route path="/" element ={<MainPage/>} />    
             <Route path="/profile" element ={<MyProfilePage/>} />
             <Route path="/auth" element ={<AuthorizationPage/>} /> 
+            <Route path="/change_password" element ={<ChangePasswordPage/>} /> 
             <Route path="/description" element ={<DescriptionPage/>} /> 
             <Route path="*" element ={<ErrPage/>} /> 
         </Routes>
