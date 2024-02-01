d4e41451845d5a64b55842238489eeae8888e96f
selecter git user <git-user@selectel.ru>
2024-02-01 20:38:17 +0800

    Редактировал

--- src/pages/Yoga.jsx
+++ src/pages/Yoga.jsx
@@ -3,18 +3,22 @@ import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import styleBody from "../styleBody";
 import BlackLogo from "../components/Logo/BlackLogo";
-import courseImg from '../img/skill card 17.png';
-import purposes from '../img/Group 48096488.png';
-import iogaNaw from '../img/iogaNaw.png';
-import iogaNew from '../img/iogaNew.png';
-import info from '../img/info.png';
-import info_button from '../img/info_button.svg'
-import buttonImage from '../img/Group 48096487.svg';
+import Fon from "../img/Group 48096400.svg"
+import Fon1 from "../img/Subtract.svg"
+import one from "../img/1.png"
+import two from "../img/2.png"
+import ofre from "../img/3.png"
 import styles from './css/ioga.module.css';
 import Burger from '../components/Burger';
 import Modal from '../components/Modal';
+import buttonImage from '../img/Group 48096487.svg';
+import info from '../img/info.png';
+import info_button from '../img/info_button.svg'
+
+
 
 export default function DescriptionPage() {
+    
     const [isOpenModalNext, setIsOpenModalNext] = useState(false); // Инициализация состояния модального окна
     const { id } = useParams(); // Получение параметра маршрута
     const courses = useSelector(state => state.store.trainingsArray); // Получение данных из Redux store
@@ -39,27 +43,50 @@ export default function DescriptionPage() {
             {courseData && (
                 <div>
                     <div className={styles.course__page_image}>
-                        <img src={courseData.ab1c3f} alt="ab1c3f" />
+                        <img src={Fon1} alt="Fon" />
+                        <div className={styles.imagetext}>
+                        <h2>{courseData.nameRU}</h2>
+                        </div>
                     </div>
                     <div className={styles.center_text}>
                         <h2>Подойдет для вас, если:</h2>
                         <div className={styles.course__ioga_image}>
-                            <img src={courseData.purposes} alt="purposes" />
+                            <div className={styles.course__ioga}>
+                                <img src={one} alt="one" className={styles.yourImageClass}/>
+                                <h3>{courseData.fitting[0]}</h3>
+                            </div>
+                            <div className={styles.course__ioga}>
+                                <img src={two} alt="two" className={styles.yourImageClass}/>
+                                <h3>{courseData.fitting[1]}</h3>
+                            </div>
+                            <div className={styles.course__ioga}>
+                                <img src={ofre} alt="ofre" className={styles.yourImageClass}/>
+                                <h3>{courseData.fitting[2]}</h3>
+                            </div>
                         </div>
                     </div>
                     <div className={styles.course__directions_text}>
                         <h2>Направления</h2>
                         <div className={styles.course__directions_image}>
-                            <img src={courseData.iogaNaw} alt="iogaNaw" />
-                            <img src={courseData.iogaNew} alt="iogaNew" />
+                            <ul className={styles.spisock}>
+                                <li>{courseData.directions[0]}</li>
+                                <li>{courseData.directions[1]}</li>
+                                <li>{courseData.directions[2]}</li>
+                            </ul>
+
+                            <ul className={styles.spisock}>
+                                <li>{courseData.directions[3]}</li>
+                                <li>{courseData.directions[4]}</li>
+                                <li>{courseData.directions[5]}</li>
+                            </ul>
                         </div>
                     </div>
                     <div className={styles.course__directions_info}>
-                        <img src={courseData.info} alt="info" />
+                        <img src={info} alt="info" />
                     </div>
                     <div className={styles.info_block}>
                         <h2 className={styles.info_text}>
-                            <img src={courseData.info_button} alt="info_button" />
+                            <img src={info_button} alt="info_button" />
                         </h2>
                         <div className={styles.button}>
                             {userIsRegistered ? ( // Проверка зарегистрирован ли пользователь
@@ -73,7 +100,7 @@ export default function DescriptionPage() {
                             )}
                         </div>
                         <div className={styles.info_image}>
-                            <img src={courseData.buttonImage} alt="buttonImage" />
+                            <img src={buttonImage} alt="buttonImage" />
                         </div>
                     </div>
                     <Modal isOpenModalNext={isOpenModalNext} handleModalClick={handleModalClick} selectedTraining={courseData} />
